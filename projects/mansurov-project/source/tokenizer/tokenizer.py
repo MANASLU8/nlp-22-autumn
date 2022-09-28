90382d5d5d1abd1c3a93d6a3e446b9c96ade4b7c
Юрий <38157715+kiarro@users.noreply.github.com>
2022-09-28 13:34:09 +0300

    fix

--- projects/mansurov-project/source/tokenizer/tokenizer.py
+++ projects/mansurov-project/source/tokenizer/tokenizer.py
@@ -26,25 +26,86 @@ def fix_html(text):
     text = html.unescape(text)
     return text
 
-def find_numbers(text):
-    text = re.sub(r"(\-?\d+[\,\.]\d+)", r" \b \1 \b ", text)
+def fix_escaped_dollar(text):
+    text = re.sub(r"\\\$", "$", text)
     return text
 
-def find_email(text):
-    text = re.sub(r"(([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*]))", r" \b \1 \b ", text)
+def fix_backslash(text):
+    text = re.sub(r"\\\\", "\n", text)
+    text = re.sub(r"\\", " ", text)
     return text
 
-def find_phone(text):
-    text = re.sub(r"(((\+?\d+ ?)?((\d+[\- ]?)+ ?((\d+[\- ]?){2,})))|((\+?\d+ ?)?(\((\+?\d+[\- ]?)+\) ?((\d+[\- ]?){2,})))|((\d+[\- ]?){2,})|(\+?\d+))", r" \b \1 \b ", text)
-    return text
+def find_numbers(parts):
+    # print(parts)
+    parts_ = []
+    k = True
+    for text in parts:
+        k = not k
+        if k:
+            parts_.append(text)
+            continue
+        text = re.sub(r"(\-?\d+[\,\.]\d+)", r" \b \1 \b ", text)
+        parts_.extend(text.split("\x08"))
+    return parts_
 
-def find_url(text):
-    text = re.sub(r"(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))", r" \b \1 \b ", text)
-    return text
+def find_email(parts):
+    # print(parts)
+    parts_ = []
+    k = True
+    for text in parts:
+        k = not k
+        if k:
+            parts_.append(text)
+            continue
+        text = re.sub(r"(([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*]))", r" \b \1 \b ", text)
+        parts_.extend(text.split("\x08"))
+    return parts_
+
+def find_phone(parts):
+    # print(parts)
+    parts_ = []
+    k = True
+    for text in parts:
+        k = not k
+        if k:
+            parts_.append(text)
+            continue
+        text = re.sub(r"(\s|^|\,)((((\+?\d+ ?)?((\d+[\- ]?)+ ?((\d+[\- ]?){2,})))|((\+?\d+ ?)?(\((\+?\d+[\- ]?)+\) ?((\d+[\- ]?){2,})))|((\d+[\- ]?){2,})|(\+?\d+)))(\s|$|\,)", r" \b \2 \b ", text)
+        parts_.extend(text.split("\x08"))
+    return parts_
+
+def find_url(parts):
+    # print(parts)
+    parts_ = []
+    k = True
+    for text in parts:
+        k = not k
+        if k:
+            parts_.append(text)
+            continue
+        text = re.sub(r"(?i)\b((?:https?:(?:\/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:\w+)\/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:\w+)\b\/?(?!@)))", r" \b \1 \b ", text)
+        parts_.extend(text.split("\x08"))
+    return parts_
+
+def find_short(parts):
+    # print(parts)
+    parts_ = []
+    k = True
+    for text in parts:
+        k = not k
+        if k:
+            parts_.append(text)
+            continue
+        # print(text)
+        text = re.sub(r"(\w+\.\'\w+)|(\w+\.)[\t ]+([^A-Z]+)|(\w+\.)[^\s\n]", r" \b \1 \2 \4 \b \3 ", text)
+        # print(text)
+        parts_.extend(text.split("\x08"))
+    return parts_
 
 # tokenize sentence
 def tokenize_sentence(sentence):
-    sentence = re.sub(r"(\.\.\.|[\.\,!\?\-\:\;\\\*\$\"])", r" \1 ", sentence)
+    # print(sentence)
+    sentence = re.sub(r"(\.\.\.|[\.\,!\?\-\:\;\\\*\$\"\(\)])", r" \1 ", sentence)
     tokens = re.split(r"\s+", sentence)
     tokens = [token for token in tokens if token != '']
     return tokens
@@ -79,16 +140,27 @@ def split_sentences(words):
     return sentences
 
 def tokenize(sentence):
-    _ = sentence
-    _ = fix_html(_)
-    _ = find_url(_)
-    _ = find_email(_)
-    _ = find_phone(_)
-    _ = find_numbers(_)
-    _ = tokenize_sentence(_)
-    _ = combine_tokens(_)
-    _ = split_sentences(_)
-    return _
+    text = sentence
+    
+    text = fix_html(text)
+    text = fix_escaped_dollar(text)
+    text = fix_backslash(text)
+    
+    parts = [text]
+    
+    parts = find_email(parts)
+    parts = find_url(parts)
+    parts = find_numbers(parts)
+    parts = find_phone(parts)
+    parts = find_short(parts)
+    
+    text = "\x08".join(parts)
+    
+    tokens = tokenize_sentence(text)
+    tokens = combine_tokens(tokens)
+    sentences = split_sentences(tokens)
+    
+    return sentences
 
 def stem_(tokens):
     stemmed = list(map(stemmer.stem, tokens))
@@ -138,8 +210,15 @@ def read_file(file, rows = 100):
     print(f'\nProcessed {line_count} rows')
     return 
     
+    
+def fix_all(text):
+    text = fix_html(text)
+    text = fix_escaped_dollar(text)
+    text = fix_backslash(text)
+    return text    
+
 if __name__ == "__main__":
-    # print(tokenize_sentence(""))
+    # print(tokenize(""))
     # print(tokenize_sentence("This is bad. Really bad (yeah), true. Very bad!"))
     # print(split_sentences(tokenize_sentence("This is bad. Really bad (yeah), true. Very bad!")))
     # print("\n\n")
@@ -153,12 +232,45 @@ if __name__ == "__main__":
     # t1 = "\\Zawodny  threads off of Scoble  on the IE issue:\\""I have to say, when I first read that I nearly fell off my chair laughing. I was\thinking ""how stupid ARE these IE guys?!?!?!"" But we all know that Microsoft is\full of smart people who care about what they're doing. So something really\doesn't compute here.""\\""Last time I checked, IE wasn't even close to feature parity with Mozilla's\browsers. No popup blocking, no tabbed browsing, etc.""\\""Does the IE team really not know what their product is missing?""\\Perhaps.  It's highly likely that they just don't know.\\The bigger issue here is that Microsoft products can't fail and they can't\succeed.  Microsoft has 40-50 billion in the bank.  There ...\\"
     # print(tokenize_sentence(fix_html(t1)))
     
+    # print(tokenize("s226.223pace.k@g.com"))
+    
     # t1 = "Russian Alien Spaceship Claims Raise Eyebrows, Skepticism (SPACE.com). SPACE.com - An expedition of Russian researchers claims to have found evidence that an \  alien spaceship had something to do with a huge explosion over Siberia in 1908. \  Experts in asteroids and comets have long said the massive blast was caused \  by a space rock."
     # print(tokenize(t1))
+    
+    # print("\n\n")
+    
+    # t1 = r"EUROPES biggest defence contractors will have the chance to bid for a \$23 billion (12 billion) air refuelling contract for the US Air Force that has been withdrawn from Boeing."
+    # print(tokenize(t1))
+    # print("\n\n")
+    
+    # t1 = r"Reuters - A group of technology companies\including Texas Instruments Inc. (TXN.N), STMicroelectronics\(STM) and Broadcom Corp. (BRCM.O), on Thursday said they\will propose a new wireless networking standard up to 10 times\the speed of the current generation."
+    # print(tokenize(t1))
+    # print("\n\n")
+    
+    # print(tokenize("Master (a.k.a. lord)."))
+    # print(tokenize("My test sentence. Second sentence"))
+    
+    # print(tokenize("Email: some.mail-my.h@mail.ru."))
+    
+    # t1 = r"\\I've been a big fan of Log4J  for a while \ now but haven't migrated any code\over for one central reason.  The following line of code:\\    final static Logger logger = Logger.getLogger( ""some.name"" );\\... is amazingly ugly and difficult to work with.\\Most people use Log4J with a logger based on the classname:\\So we would probably see:\\    static Logger logger = Logger.getLogger( ""org.apache.commons.feedparser.locate.FeedLocator"" );\\Which is amazingly verbose.  A lot of developers shorten this to:\\    static Logger logger = Logger.getLogger( FeedLocator.class );\\But this still leaves us with cut and paste errors.\\What if we could just reduce it to:\\    static Logger logger = Logger.g ...\\"
+    # print(tokenize(t1))
+    # print("\n\n")
+    
+    # t1 = r"The \$41 billion merger between Cingular Wireless LLC and AT T Wireless Services Inc. won approval from the Federal Communications Commission Friday, according to federal sources close to the agency. &lt;BR&gt;\&lt;FONT face=""verdana,MS Sans Serif,arial,helvetica"" size=""-2""\ color=""#666666""&gt;&lt;B&gt;-The Washington Post&lt;/B&gt;&lt;/FONT&gt;"
+    # print(tokenize(t1))
+    # print("\n\n")
+    
+    # t1 = r"AP - Steve Gleason of the New Orleans Saints was fined  #36;5,000 by the NFL on Wednesday after being thrown out of last week's game with Carolina for punching the Panthers' Kemp Rasmussen at the end of a kickoff return."
+    # print(tokenize(t1))
+    # print("\n\n")
+    
+    # print("\\\\$ \\\$ \\$ \$ $")
+    # print(fix_escaped_dollar("\\\\$ \\\$ \\$ \$ $"))
     
     # read_file('projects/mansurov-project/assets/train.csv')
     
     read_and_annotate('test', rows = -1)
+    read_and_annotate('train', rows = -1)
     
     # f_out = f'projects/mansurov-project/assets/annotated-corpus/train/3/test.tsv'
     # annotate("Russian Alien Spaceship Claims Raise Eyebrows, Skepticism (SPACE.com). SPACE.com - An expedition of Russian researchers claims to have found evidence that an \  alien spaceship had something to do with a huge explosion over Siberia in 1908. \  Experts in asteroids and comets have long said the massive blast was caused \  by a space rock.", f_out)
